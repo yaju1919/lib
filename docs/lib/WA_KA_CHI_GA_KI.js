4bc6abdbab98f8f6301c40a7fda8689db689c3ec
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-16 11:45:00 +0900

    Create WA_KA_CHI_GA_KI.js

---
+++ docs/lib/WA_KA_CHI_GA_KI.js
@@ -0,0 +1,24 @@
+var WA_KA_CHI_GA_KI = (function(){
+    var type_reg = new RegExp((function(){
+        var array = [
+            'ぁ-ん',
+            'ァ-ヴーｱ-ﾝﾞｰ',
+            '一-龠々〆ヵヶ',
+            'a-zA-Zａ-ｚＡ-Ｚ',
+            '0-9０-９',
+            '・、。,\\.？\\?！「」!',
+            '　 ',
+            '\n'
+        ];
+        array.push('^' + array.join(''));
+        return array.map(str => "[" + str + "]+").join('|');
+    })(), 'g');
+    return function(_str, _num){
+        if(typeof _str !== "string") return null;
+        if (0 < Number(_num) && !isNaN(_num)) {
+            var reg = new RegExp('(.|\n){1,' + _num + '}', 'g');
+            return _str.match(reg) || null;
+        }
+        return _str.match(type_reg) || null;
+    }
+})();
