11231da3620f8629a763131e2186d4c667e1562d
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-29 22:26:23 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -13,6 +13,34 @@
         var a = window[v];
         $("<div>").text(v + ':' + (a ? "OK" : "Error")).appendTo(document.body);
     });
+    //--
+    (function(window,undefined){
+    var type_reg = new RegExp((function(){
+        var array = [
+            'ぁ-ん',
+            'ァ-ヴーｱ-ﾝﾞｰ',
+            '一-龠々〆ヵヶ',
+            'a-zA-Zａ-ｚＡ-Ｚ',
+            '0-9０-９',
+            '・、。,\\.？\\?！「」!',
+            '　 ',
+            '\n'
+        ];
+        array.push('^' + array.join(''));
+        return array.map(str => "[" + str + "]+").join('|');
+    })(), 'g');
+    function main(_str, _num){
+        if(typeof _str !== "string") return null;
+        if (0 < Number(_num) && !isNaN(_num)) {
+            var reg = new RegExp('(.|\n){1,' + _num + '}', 'g');
+            return _str.match(reg) || null;
+        }
+        return _str.match(type_reg) || null;
+    }
+    window.WA_KA_CHI_GA_KI = main;
+})(window);
+    alert(!!window.WA_KA_CHI_GA_KI)
+    //--
     yaju1919.addInputText(h,{
         title: "てすや"
     })
