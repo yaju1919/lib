4a945435450310401a66bf3069d423b0e605e6ae
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-29 21:45:03 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -5,14 +5,14 @@
     alert(typeof TinySegmenter + typeof getRoman + typeof $);
     var func1 = (function(){
         function func2(){}
-return func2;
-})();
+        return func2;
+    })();
     alert(func1);
     var func3 = (function(){
         function func3(){}
-return func3;
-})();
-    alert(fff);
+        return func3;
+    })();
+    alert(func3);
     alert("おわり");
     yaju1919.addInputText(h,{
         title: "てすや"
