1d63c8dd84a2bafb992a948d02318d0708ca774c
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-29 21:39:28 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,12 +3,10 @@
     var h = $("<div>").appendTo(document.body);
     $("<h1>",{text:"test"}).appendTo(h);
     alert(typeof TinySegmenter + typeof getRoman + typeof $);
-    alert(1);
-    alert(function(){});
-    alert(2);
-    var test = function(){};
-    alert(3);
-    alert(test);
+    var unk = (function(){
+return "うんこ";
+})();
+    alert(unk);
     alert("おわり");
     yaju1919.addInputText(h,{
         title: "てすや"
