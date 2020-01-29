3487479f30bf8a3b61699ec0f3a1a8484fe9b965
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-30 00:32:44 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     var h = $("<div>").appendTo(document.body);
-    $("<h1>",{text:"test"}).appendTo(h);
+    $("<h1>",{text:"お使いの端末でどのライブラリが使え無いのかチェック"}).appendTo(h);
     [
         "jQuery",
         "TinySegmenter",
@@ -13,7 +13,36 @@
         var a = window[v];
         $("<div>").text(v + ':' + (a ? "OK" : "Error")).appendTo(document.body);
     });
-    yaju1919.addInputText(h,{
-        title: "てすや"
-    })
+    $("<h2>").text("JSエディタ").appendTo(h);
+    function console_log(x,color){
+        var str = '';
+        switch(typeof x){
+            case "object":
+                str = '{' + Object.keys(x).map(function(k){
+                    return k + ':' + String(x[k])
+                }).join(',') + '}';
+                break;
+            default:
+                str = String(x);
+                break;
+        }
+        $("<div>").text(str).css({backgroundColor:color}).appendTo(output);
+    };
+    if(!window.console) window.console = {};
+    console.log = function(x){ console_log(x,"gray") };
+    console.error = function(x){ console_log(x,"pink") };
+    console.warn = function(x){ console_log(x,"yellow") };
+    console.info = function(x){ console_log(x,"lightblue") };
+    $("<button>").appendTo(h).click(function(){
+        output.empty();
+        var v = input();
+        try {
+            console.log((0, eval)(v));
+        }
+        catch (e) {
+            console.error(e);
+        }
+    });
+    var input = $("<textarea>").appendTo(h);
+    var output = $("<div>").appendTo(h);
 })();
