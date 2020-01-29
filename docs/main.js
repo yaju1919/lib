a66b2a12d077b0646b355436d973ffc5e45ddc79
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-29 20:50:32 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@
     'use strict';
     var h = $("<div>").appendTo(document.body);
     $("<h1>",{text:"test"}).appendTo(h);
-    alert("さとる");
+    alert(yaju1919.hello());
     yaju1919.addInputText(h,{
         title: "てすや"
     })
