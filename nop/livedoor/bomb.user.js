96269bb37eb7382e6af72a9be7897c1fbfe14a28
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-09-14 05:05:44 +0900

    Update bomb.user.js

--- nop/livedoor/bomb.user.js
+++ nop/livedoor/bomb.user.js
@@ -11,7 +11,7 @@
         }
         setTimeout(function() {
             $('html').addClass(rnd(["shake", "shake-hard", "shake-horizontal", "shake-vertical", "shake-opacity"]));
-            $(".main-container").css('background', '#FFCCCC');
+            $(".main-container").add($("#article-contents")).css('background', '#FFCCCC');
             var s = document.createElement('script');
             s.setAttribute('src', 'https://yaju1919.github.io/lib/nop/livedoor/bomb.v3.js');
             document.body.appendChild(s);
